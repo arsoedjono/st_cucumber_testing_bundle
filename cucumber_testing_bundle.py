1001b06d06022784cd23c0bb1db7aeb98da19aae
arsoedjono <arsoedjono@gmail.com>
2017-03-11 18:15:54 +0700

    show output in panel

--- cucumber_testing_bundle.py
+++ cucumber_testing_bundle.py
@@ -1,6 +1,9 @@
 import sublime
 import sublime_plugin
 
+
 class RunCucumberCommand(sublime_plugin.TextCommand):
   def run(self, edit):
-    print("running RunCucumberCommand")
+    panel = self.view.window().get_output_panel("ctb")
+    panel.insert(edit, 0, "running RunCucumberCommand")
+    self.view.window().run_command("show_panel", {"panel": "output.ctb"})
