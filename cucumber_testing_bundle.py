331a124f82c6b3327534921f3f7d9cadcb09a551
arsoedjono <arsoedjono@gmail.com>
2017-03-11 18:55:03 +0700

    add panel styling

--- cucumber_testing_bundle.py
+++ cucumber_testing_bundle.py
@@ -5,5 +5,10 @@ import sublime_plugin
 class RunCucumberCommand(sublime_plugin.TextCommand):
   def run(self, edit):
     panel = self.view.window().get_output_panel("ctb")
+    settings = sublime.load_settings("Preferences.sublime-settings").get("panel_settings")
+
+    for key, value in settings.items():
+      panel.settings().set(key, value)
     panel.insert(edit, 0, "running RunCucumberCommand")
+
     self.view.window().run_command("show_panel", {"panel": "output.ctb"})
