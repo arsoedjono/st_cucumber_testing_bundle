f3cb1953e7abedbdb04eabf0df764929ca49f412
arsoedjono <arsoedjono@gmail.com>
2017-03-16 00:19:33 +0700

    user preferable settings

--- libs/settings.py
+++ libs/settings.py
@@ -4,5 +4,14 @@ class Settings(object):
   def __init__(self, key):
     self.key = key
 
+  def _default_settings(self):
+    return sublime.load_settings("Preferences.sublime-settings")
+
+  def _user_settings(self):
+    return sublime.load_settings("CucumberTestingBundle.sublime-settings")
+
   def retrieve(self):
-    return sublime.load_settings("Preferences.sublime-settings").get(self.key)
+    return self._user_settings().get(
+      self.key,
+      self._default_settings().get(self.key, None)
+    )
