2a81fc1996d2fefbff960c3e10775e0ac11a0d95
arsoedjono <arsoedjono@gmail.com>
2017-03-29 20:08:22 +0700

    run current file

--- libs/job.py
+++ libs/job.py
@@ -5,6 +5,9 @@ from libs.settings import Settings
 import sublime
 
 class Job(object):
+  NOT_VALID_CUCUMBER_FILE = "Not a valid cucumber file (.feature)"
+  NOT_EXECUTABLE = "No such file or directory - features. You can use `cucumber --init` to get started."
+
   def __init__(self, view, edit):
     self.view = view
     self.window = view.window()
@@ -14,14 +17,18 @@ class Job(object):
   def _file_name(self):
     return self.view.file_name()
 
+  def _is_cucumber_file(self, file_name):
+    return file_name.endswith(".feature")
+
   def _project_name(self):
     return sublime.active_window().folders()[0]
 
-  def is_executable(self):
-    flag = self.root()
+  def is_executable(self, file_name = None):
+    flag = self._is_cucumber_file(file_name) if file_name else self.root()
 
     if not flag:
-      self.panel.write("No such file or directory - features. You can use `cucumber --init` to get started.")
+      msg = "{0} -- {1}".format(self.NOT_VALID_CUCUMBER_FILE, file_name) if file_name else self.NOT_EXECUTABLE
+      self.panel.write(msg)
       self.panel.display_panel()
 
     return flag
