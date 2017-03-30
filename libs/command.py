31d9d2c5e4f7f6e47720876a02e467eb3eb399ea
arsoedjono <arsoedjono@gmail.com>
2017-03-30 17:14:13 +0700

    run last test

--- libs/command.py
+++ libs/command.py
@@ -2,9 +2,12 @@ from libs.settings import Settings
 from libs.root import Root
 
 import os
+import sublime
 
 class Command(object):
   GEMFILE_NAME = "Gemfile"
+  SETTINGS_FILE = "CucumberTestingBundle.last"
+  LAST_RUN_KEY = "command"
 
   def __init__(self, job):
     self.job = job
@@ -17,10 +20,18 @@ class Command(object):
     file_dir = os.path.expanduser(Settings("rvm_path").retrieve())
     if file_dir and os.path.isfile(file_dir): return "{0} -S".format(file_dir)
 
+  def last_command(self):
+    return sublime.load_settings(self.SETTINGS_FILE).get(self.LAST_RUN_KEY)
+
   def retrieve(self, target = None, line = None):
     return " ".join(filter(None, [
       self._get_rvm(),
       self._get_bundler(),
       Settings("cucumber_command").retrieve(),
-      ":".join(filter(None, [target, line]))
+      ":".join(filter(None, [target, line])) or None
     ]))
+
+  def save(self, command):
+    settings = sublime.load_settings(self.SETTINGS_FILE)
+    settings.set(self.LAST_RUN_KEY, command)
+    sublime.save_settings(self.SETTINGS_FILE)
